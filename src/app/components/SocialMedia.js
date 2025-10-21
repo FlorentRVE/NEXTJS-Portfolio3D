2884dbe4fd9c9268774505987683b197a3f78c30
florent.rve <florent.rve@gmail.com>
2025-10-21 17:54:44 +0400

    Modif lien Insta

--- src/app/components/SocialMedia.js
+++ src/app/components/SocialMedia.js
@@ -5,32 +5,32 @@ const SocialMedia = () => {
     <div className="modal-projects-wrapper">
       <div className="modal-socials">
         {/* INSTAGRAM */}
-          <a
-            className="modal-socials-link"
-            href="https://www.instagram.com/florent.rve/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/Instagram.svg"
-              alt="TikTok"
-              className="modal-socials-image"
-            />
-          </a>
+        <a
+          className="modal-socials-link"
+          href="https://www.instagram.com/flow_rve/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/images/Instagram.svg"
+            alt="TikTok"
+            className="modal-socials-image"
+          />
+        </a>
 
         {/* TIK TOK */}
-          <a
-            className="modal-socials-link"
-            href="https://www.tiktok.com/@florent.rve"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/TikTok.svg"
-              alt="TikTok"
-              className="modal-socials-image"
-            />
-          </a>
+        <a
+          className="modal-socials-link"
+          href="https://www.tiktok.com/@florent.rve"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="/images/TikTok.svg"
+            alt="TikTok"
+            className="modal-socials-image"
+          />
+        </a>
       </div>
     </div>
   );
